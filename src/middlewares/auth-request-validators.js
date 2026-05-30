108321a5509510a7929dd679c89b904f2c574513
shakib-SKRLS <sk2724274@gmail.com>
2026-05-31 00:42:42 +0530

    added email and password validation

---
+++ src/middlewares/auth-request-validators.js
@@ -0,0 +1,11 @@
+const validateUserAuth =  (req, res, next) => {
+    if (!req.body.email || !req.body.password) {
+        return res.status(400).json({ message: 'Email and password are required' });
+    }
+    next();
+}
+
+module.exports = {
+    validateUserAuth
+
+}
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../../controllers/user-controller');
+const { AuthRequestValidators } = require('../../middlewares/index');
 
-router.post('/signup', userController.create);
-router.post('/signin', userController.signIn);
+router.post('/signup', AuthRequestValidators.validateUserAuth, userController.create);
+router.post('/signin', AuthRequestValidators.validateUserAuth, userController.signIn);
 
 module.exports = router;
